517402c6738fc4ac9c9ab82b3bac04776e495825
faiz000208 <2021112699@student.uitm.edu.my>
2021-12-09 03:07:49 +0800

    connection changed

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','sos-edited-new')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','soryuDatabase')or die("Could not connect to mysql".mysqli_error($con));
